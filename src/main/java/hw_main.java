6c15fa338c3160c6e024d5c0f4d027c81c6ca746
Joe Smith <jsmitty54@verizon.net>
2015-01-20 22:52:55 -0500

    First Version
    First version to test GIT

---
+++ src/main/java/hw_main.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Joe on 1/20/2015.
+ */
+public class hw_main {
+
+    public static void main (String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!");
+    }
+
+
+
+}
